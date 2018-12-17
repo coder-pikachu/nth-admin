0790c01465f1e851932a2b4571dde4f154201ed2
Sangram Mohite <mohite.sangram@hotmail.com>
2018-12-17 14:00:44 +0000

    Vue Component added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,29 @@
 import VueRouter from 'vue-router'
-import BoardList from '@/components/BoardList'
-import ShowBoard from '@/components/ShowBoard'
-import AddBoard from '@/components/AddBoard'
-import EditBoard from '@/components/EditBoard'
+import PuzzleList from '@/components/PuzzleList'
+import ShowPuzzle from '@/components/ShowPuzzle'
+import AddPuzzle from '@/components/AddPuzzle'
+import EditPuzzle from '@/components/EditPuzzle'
+
+export default new VueRouter({
+    routes: [{
+            path: '/',
+            name: 'PuzzleList',
+            component: "PuzzleList"
+        },
+        {
+            path: '/show-puzzle/:id',
+            name: 'ShowPuzzle',
+            component: "ShowPuzzle"
+        },
+        {
+            path: '/add-puzzle',
+            name: 'AddPuzzle',
+            component: "AddPuzzle"
+        },
+        {
+            path: '/edit-puzzle/:id',
+            name: 'EditPuzzle',
+            component: "EditPuzzle"
+        }
+    ]
+})
