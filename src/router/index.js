4a52f58fc57bb6b2f67638a5fbbcab7347f0b2be
Sangram Mohite <mohite.sangram@hotmail.com>
2018-12-17 13:52:12 +0000

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import VueRouter from 'vue-router'
+import BoardList from '@/components/BoardList'
+import ShowBoard from '@/components/ShowBoard'
+import AddBoard from '@/components/AddBoard'
+import EditBoard from '@/components/EditBoard'
